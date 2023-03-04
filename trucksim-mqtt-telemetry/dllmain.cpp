331fc0f455da014f005fc7d791e055a8217c9e2b
Ondra Červenka <gogo.lejzr@gmail.com>
2023-03-04 23:00:33 +0100

    for now removed logging into the file

--- trucksim-mqtt-telemetry/dllmain.cpp
+++ trucksim-mqtt-telemetry/dllmain.cpp
@@ -38,12 +38,6 @@ FILE* log_file = NULL;
 bool output_paused = true;
 
 /**
- * @brief Should we print the data header next time
- * we are printing the data?
- */
-bool print_header = true;
-
-/**
  * @brief Last timestamp we received.
  */
 scs_timestamp_t last_timestamp = static_cast<scs_timestamp_t>(-1);
@@ -73,54 +67,6 @@ struct telemetry_state_t
  * @brief Function writting message to the game internal log.
  */
 scs_log_t game_log = NULL;
-
-// Management of the log file.
-
-bool init_log(void)
-{
-	if (log_file) {
-		return true;
-	}
-	log_file = fopen("telemetry-mqtt-test.log", "wt");
-	if (!log_file) {
-		return false;
-	}
-	fprintf(log_file, "Log opened\n");
-	return true;
-}
-
-void finish_log(void)
-{
-	if (!log_file) {
-		return;
-	}
-	fprintf(log_file, "Log ended\n");
-	fclose(log_file);
-	log_file = NULL;
-}
-
-void log_print(const char* const text, ...)
-{
-	if (!log_file) {
-		return;
-	}
-	va_list args;
-	va_start(args, text);
-	vfprintf(log_file, text, args);
-	va_end(args);
-}
-
-void log_line(const char* const text, ...)
-{
-	if (!log_file) {
-		return;
-	}
-	va_list args;
-	va_start(args, text);
-	vfprintf(log_file, text, args);
-	fprintf(log_file, "\n");
-	va_end(args);
-}
 
 // Handling of individual events.
 
@@ -164,143 +110,11 @@ SCSAPI_VOID telemetry_frame_end(const scs_event_t UNUSED(event), const void* con
 	}
 
 	// The header.
-
-	if (print_header) {
-		print_header = false;
-		log_line("timestamp[us];raw rendering timestamp[us];raw simulation timestamp[us];raw paused simulation timestamp[us];heading[deg];pitch[deg];roll[deg];speed[m/s];rpm;gear");
-	}
-
-	// The data line.
-
-	log_print("%" SCS_PF_U64 ";%" SCS_PF_U64 ";%" SCS_PF_U64 ";%" SCS_PF_U64, telemetry.timestamp, telemetry.raw_rendering_timestamp, telemetry.raw_simulation_timestamp, telemetry.raw_paused_simulation_timestamp);
-	if (telemetry.orientation_available) {
-		log_print(";%f;%f;%f", telemetry.heading, telemetry.pitch, telemetry.roll);
-	}
-	else {
-		log_print(";---;---;---");
-	}
-	log_line(
-		";%f;%f;%d",
-		telemetry.speed,
-		telemetry.rpm,
-		telemetry.gear
-	);
 }
 
 SCSAPI_VOID telemetry_pause(const scs_event_t event, const void* const UNUSED(event_info), const scs_context_t UNUSED(context))
 {
 	output_paused = (event == SCS_TELEMETRY_EVENT_paused);
-	if (output_paused) {
-		log_line("Telemetry paused");
-	}
-	else {
-		log_line("Telemetry unpaused");
-	}
-	print_header = true;
-}
-
-void telemetry_print_attributes(const scs_named_value_t* const attributes)
-{
-	for (const scs_named_value_t* current = attributes; current->name; ++current) {
-		log_print("  %s", current->name);
-		if (current->index != SCS_U32_NIL) {
-			log_print("[%u]", static_cast<unsigned>(current->index));
-		}
-		log_print(" : ");
-		switch (current->value.type) {
-		case SCS_VALUE_TYPE_INVALID: {
-			log_line("none");
-			break;
-		}
-		case SCS_VALUE_TYPE_bool: {
-			log_line("bool = %s", current->value.value_bool.value ? "true" : "false");
-			break;
-		}
-		case SCS_VALUE_TYPE_s32: {
-			log_line("s32 = %d", static_cast<int>(current->value.value_s32.value));
-			break;
-		}
-		case SCS_VALUE_TYPE_u32: {
-			log_line("u32 = %u", static_cast<unsigned>(current->value.value_u32.value));
-			break;
-		}
-		case SCS_VALUE_TYPE_s64: {
-			log_line("s64 = %" SCS_PF_S64, current->value.value_s64.value);
-			break;
-		}
-		case SCS_VALUE_TYPE_u64: {
-			log_line("u64 = %" SCS_PF_U64, current->value.value_u64.value);
-			break;
-		}
-		case SCS_VALUE_TYPE_float: {
-			log_line("float = %f", current->value.value_float.value);
-			break;
-		}
-		case SCS_VALUE_TYPE_double: {
-			log_line("double = %f", current->value.value_double.value);
-			break;
-		}
-		case SCS_VALUE_TYPE_fvector: {
-			log_line(
-				"fvector = (%f,%f,%f)",
-				current->value.value_fvector.x,
-				current->value.value_fvector.y,
-				current->value.value_fvector.z
-			);
-			break;
-		}
-		case SCS_VALUE_TYPE_dvector: {
-			log_line(
-				"dvector = (%f,%f,%f)",
-				current->value.value_dvector.x,
-				current->value.value_dvector.y,
-				current->value.value_dvector.z
-			);
-			break;
-		}
-		case SCS_VALUE_TYPE_euler: {
-			log_line(
-				"euler = h:%f p:%f r:%f",
-				current->value.value_euler.heading * 360.0f,
-				current->value.value_euler.pitch * 360.0f,
-				current->value.value_euler.roll * 360.0f
-			);
-			break;
-		}
-		case SCS_VALUE_TYPE_fplacement: {
-			log_line(
-				"fplacement = (%f,%f,%f) h:%f p:%f r:%f",
-				current->value.value_fplacement.position.x,
-				current->value.value_fplacement.position.y,
-				current->value.value_fplacement.position.z,
-				current->value.value_fplacement.orientation.heading * 360.0f,
-				current->value.value_fplacement.orientation.pitch * 360.0f,
-				current->value.value_fplacement.orientation.roll * 360.0f
-			);
-			break;
-		}
-		case SCS_VALUE_TYPE_dplacement: {
-			log_line(
-				"dplacement = (%f,%f,%f) h:%f p:%f r:%f",
-				current->value.value_dplacement.position.x,
-				current->value.value_dplacement.position.y,
-				current->value.value_dplacement.position.z,
-				current->value.value_dplacement.orientation.heading * 360.0f,
-				current->value.value_dplacement.orientation.pitch * 360.0f,
-				current->value.value_dplacement.orientation.roll * 360.0f
-			);
-			break;
-		}
-		case SCS_VALUE_TYPE_string: {
-			log_line("string = %s", current->value.value_string.value);
-			break;
-		}
-		default: {
-			log_line("unknown");
-			break;
-		}
-		}
-	}
 }
 
 SCSAPI_VOID telemetry_configuration(const scs_event_t event, const void* const event_info, const scs_context_t UNUSED(context))
@@ -308,11 +122,6 @@ SCSAPI_VOID telemetry_configuration(const scs_event_t event, const void* const e
 	// Here we just print the configuration info.
 
 	const struct scs_telemetry_configuration_t* const info = static_cast<const scs_telemetry_configuration_t*>(event_info);
-	log_line("Configuration: %s", info->id);
-
-	telemetry_print_attributes(info->attributes);
-
-	print_header = true;
 }
 
 SCSAPI_VOID telemetry_gameplay_event(const scs_event_t event, const void* const event_info, const scs_context_t UNUSED(context))
@@ -320,11 +129,6 @@ SCSAPI_VOID telemetry_gameplay_event(const scs_event_t event, const void* const 
 	// Here we just print the event info.
 
 	const struct scs_telemetry_gameplay_event_t* const info = static_cast<const scs_telemetry_gameplay_event_t*>(event_info);
-	log_line("Gameplay event: %s", info->id);
-
-	telemetry_print_attributes(info->attributes);
-
-	print_header = true;
 }
 
 // Handling of individual channels.
@@ -386,16 +190,10 @@ SCSAPI_RESULT scs_telemetry_init(const scs_u32_t version, const scs_telemetry_in
 	}
 
 	const scs_telemetry_init_params_v101_t* const version_params = static_cast<const scs_telemetry_init_params_v101_t*>(params);
-	if (!init_log()) {
-		version_params->common.log(SCS_LOG_TYPE_error, "Unable to initialize the log file");
-		return SCS_RESULT_generic_error;
-	}
 
 	// Check application version. Note that this example uses fairly basic channels which are likely to be supported
 	// by any future SCS trucking game however more advanced application might want to at least warn the user if there
 	// is game or version they do not support.
-
-	log_line("Game '%s' %u.%u", version_params->common.game_id, SCS_GET_MAJOR_VERSION(version_params->common.game_version), SCS_GET_MINOR_VERSION(version_params->common.game_version));
 
 	if (strcmp(version_params->common.game_id, SCS_GAME_ID_EUT2) == 0) {
 
@@ -404,14 +202,14 @@ SCSAPI_RESULT scs_telemetry_init(const scs_u32_t version, const scs_telemetry_in
 
 		const scs_u32_t MINIMAL_VERSION = SCS_TELEMETRY_EUT2_GAME_VERSION_1_00;
 		if (version_params->common.game_version < MINIMAL_VERSION) {
-			log_line("WARNING: Too old version of the game, some features might behave incorrectly");
+			// log_line("WARNING: Too old version of the game, some features might behave incorrectly");
 		}
 
 		// Future versions are fine as long the major version is not changed.
 
 		const scs_u32_t IMPLEMENTED_VERSION = SCS_TELEMETRY_EUT2_GAME_VERSION_CURRENT;
 		if (SCS_GET_MAJOR_VERSION(version_params->common.game_version) > SCS_GET_MAJOR_VERSION(IMPLEMENTED_VERSION)) {
-			log_line("WARNING: Too new major version of the game, some features might behave incorrectly");
+			// log_line("WARNING: Too new major version of the game, some features might behave incorrectly");
 		}
 	}
 	else if (strcmp(version_params->common.game_id, SCS_GAME_ID_ATS) == 0) {
@@ -421,18 +219,18 @@ SCSAPI_RESULT scs_telemetry_init(const scs_u32_t version, const scs_telemetry_in
 
 		const scs_u32_t MINIMAL_VERSION = SCS_TELEMETRY_ATS_GAME_VERSION_1_00;
 		if (version_params->common.game_version < MINIMAL_VERSION) {
-			log_line("WARNING: Too old version of the game, some features might behave incorrectly");
+			// log_line("WARNING: Too old version of the game, some features might behave incorrectly");
 		}
 
 		// Future versions are fine as long the major version is not changed.
 
 		const scs_u32_t IMPLEMENTED_VERSION = SCS_TELEMETRY_ATS_GAME_VERSION_CURRENT;
 		if (SCS_GET_MAJOR_VERSION(version_params->common.game_version) > SCS_GET_MAJOR_VERSION(IMPLEMENTED_VERSION)) {
-			log_line("WARNING: Too new major version of the game, some features might behave incorrectly");
+			// log_line("WARNING: Too new major version of the game, some features might behave incorrectly");
 		}
 	}
 	else {
-		log_line("WARNING: Unsupported game, some features or values might behave incorrectly");
+		// log_line("WARNING: Unsupported game, some features or values might behave incorrectly");
 	}
 
 	// Register for events. Note that failure to register those basic events
@@ -476,12 +274,11 @@ SCSAPI_RESULT scs_telemetry_init(const scs_u32_t version, const scs_telemetry_in
 	// Remember the function we will use for logging.
 
 	game_log = version_params->common.log;
-	game_log(SCS_LOG_TYPE_message, "Initializing telemetry log example");
+	game_log(SCS_LOG_TYPE_message, "Initializing MQTT plugin...");
 
 	// Set the structure with defaults.
 
 	memset(&telemetry, 0, sizeof(telemetry));
-	print_header = true;
 	last_timestamp = static_cast<scs_timestamp_t>(-1);
 
 	// Initially the game is paused.
@@ -501,7 +298,6 @@ SCSAPI_VOID scs_telemetry_shutdown(void)
 	// so there is no need to do that manually.
 
 	game_log = NULL;
-	finish_log();
 }
 
 // Cleanup
@@ -513,9 +309,6 @@ BOOL APIENTRY DllMain(
 	LPVOID reseved
 )
 {
-	if (reason_for_call == DLL_PROCESS_DETACH) {
-		finish_log();
-	}
 	return TRUE;
 }
 #endif
