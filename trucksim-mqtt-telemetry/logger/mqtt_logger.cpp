921fd54425d0d375acec60949044707f8475ca28
Ondra Červenka <gogo.lejzr@gmail.com>
2023-03-10 23:52:14 +0100

    json logs

--- trucksim-mqtt-telemetry/logger/mqtt_logger.cpp
+++ trucksim-mqtt-telemetry/logger/mqtt_logger.cpp
@@ -1,15 +1,22 @@
 #include "../pch.h"
 
+#include <nlohmann/json.hpp>
 #include <mqtt/client.h>
 
 #include "logger.h"
 #include "mqtt_logger.h"
 
+using json = nlohmann::json;
+
 namespace trucksim_mqtt {
 	MqttLogger::MqttLogger(mqtt::client* client) : client(client) {};
 
 	void MqttLogger::log(const char* msg, LogLevel level) {
-		auto mqtt_msg = mqtt::make_message(kLogTopic, msg);
+		json j;
+		j["msg"] = msg;
+		j["level"] = level;
+
+		auto mqtt_msg = mqtt::make_message(kLogTopic, j.dump());
 		client->publish(mqtt_msg);
 	}
 }
