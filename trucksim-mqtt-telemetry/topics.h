b9d9808a5e0d10536614f7b59675792207934cdf
Ondra Červenka <gogo.lejzr@gmail.com>
2023-03-11 00:40:36 +0100

    added some topic constants

--- trucksim-mqtt-telemetry/topics.h
+++ trucksim-mqtt-telemetry/topics.h
@@ -11,5 +11,25 @@ namespace trucksim_mqtt {
 	/// Topic used to publish plugin logs.
 	/// </summary>
 	constexpr auto kLogTopic{ "trucksim/log" };
+
+	// Event topics
+
+	constexpr auto kEventsGameplayTopic{ "trucksim/event/gameplay" };
+	constexpr auto kEventConfigTopic{ "trucksim/event/config" };
+
+	// Channel topics
+
+	// Truck channels
+
+	constexpr auto kTruckWorldPlacementTopic{ "trucksim/channel/truck/world_placement" };
+	constexpr auto kTruckSpeedTopic{ "trucksim/channel/truck/speed" };
+	constexpr auto kTruckCruiseControlTopic{ "trucksim/channel/truck/cruise_control" };
+
+	constexpr auto kTruckEngineRpmTopic{ "trucksim/channel/truck/engine/rpm" };
+	constexpr auto kTruckEngineGearTopic{ "trucksim/channel/truck/engine/gear" };
+
+	constexpr auto kTruckFuelAmountTopic{ "trucksim/channel/truck/fuel/amount" };
+	constexpr auto kTruckFuelRangeTopic{ "trucksim/channel/truck/fuel/range" };
+	constexpr auto kTruckFuelWarningTopic{ "trucksim/channel/truck/fuel/warning" };
 }
 
