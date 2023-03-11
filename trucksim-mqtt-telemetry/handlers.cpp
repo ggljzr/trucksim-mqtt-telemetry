f9833cfd6cf086d0eeebc1c8a7468c78f9e6f1c8
Ondra Červenka <gogo.lejzr@gmail.com>
2023-03-11 17:13:38 +0100

    note about handlers that require caching

--- trucksim-mqtt-telemetry/handlers.cpp
+++ trucksim-mqtt-telemetry/handlers.cpp
@@ -96,8 +96,11 @@ namespace trucksim_mqtt {
 			// it (SCS_RESULT_not_found) or if does not support the requested type
 			// (SCS_RESULT_unsupported_type). For purpose of this example we ignore the failues
 			// so the unsupported channels will remain at theirs default value.
+
+			// these handlers require some kind of caching implemented and are disabled for now
 			//version_params->register_for_channel(SCS_TELEMETRY_TRUCK_CHANNEL_world_placement, SCS_U32_NIL, SCS_VALUE_TYPE_euler, SCS_TELEMETRY_CHANNEL_FLAG_none, on_world_placement, telemetry);
-			version_params->register_for_channel(SCS_TELEMETRY_TRUCK_CHANNEL_speed, SCS_U32_NIL, SCS_VALUE_TYPE_float, SCS_TELEMETRY_CHANNEL_FLAG_none, on_speed_changed, telemetry);
+			//version_params->register_for_channel(SCS_TELEMETRY_TRUCK_CHANNEL_speed, SCS_U32_NIL, SCS_VALUE_TYPE_float, SCS_TELEMETRY_CHANNEL_FLAG_none, on_speed_changed, telemetry);
+			
 			version_params->register_for_channel(SCS_TELEMETRY_TRUCK_CHANNEL_engine_rpm, SCS_U32_NIL, SCS_VALUE_TYPE_float, SCS_TELEMETRY_CHANNEL_FLAG_none, on_rpm_changed, telemetry);
 			version_params->register_for_channel(SCS_TELEMETRY_TRUCK_CHANNEL_engine_gear, SCS_U32_NIL, SCS_VALUE_TYPE_s32, SCS_TELEMETRY_CHANNEL_FLAG_none, on_gear_changed, telemetry);
 			
