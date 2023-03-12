11ec24651c945b5c244d444019e79febb63de31f
Ondra Červenka <gogo.lejzr@gmail.com>
2023-03-12 12:23:59 +0100

    publish val for strings

--- trucksim-mqtt-telemetry/telemetry.cpp
+++ trucksim-mqtt-telemetry/telemetry.cpp
@@ -44,6 +44,7 @@ namespace trucksim_mqtt {
 	template void Telemetry::publish_val<scs_s32_t>(const scs_s32_t value, const char* topic) const;
 	// also for bool values from SCS API
 	template void Telemetry::publish_val<scs_u8_t>(const scs_u8_t value, const char* topic) const;
+	template void Telemetry::publish_val<scs_string_t>(const scs_string_t value, const char* topic) const;
 
 	void Telemetry::version_check(const scs_telemetry_init_params_v101_t* const version_params) const
 	{
