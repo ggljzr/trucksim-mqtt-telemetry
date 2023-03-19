2c183395eedf1546bb70b17e18ecc053795e8cf5
Ondra Červenka <gogo.lejzr@gmail.com>
2023-03-19 18:03:27 +0100

    attr_to_json replaces . in attr name with _

--- trucksim-mqtt-telemetry/sdk_mqtt_interop.cpp
+++ trucksim-mqtt-telemetry/sdk_mqtt_interop.cpp
@@ -29,92 +29,101 @@ namespace trucksim_mqtt {
 	void attrs_to_json(json& json, const scs_named_value_t* const attrs)
 	{
 		for (const scs_named_value_t* current = attrs; current->name; ++current) {
+
+			// replace dots in attribute name with underscores
+			// to prevent confusion in javascript,
+			// where dots would conflict with object notation
+			// TODO: it would be nice if this function would actually
+			// load names with dots recursively into JSON subkeys
+			auto name = std::string(current->name);
+			std::replace(name.begin(), name.end(), '.', '_');
+
 			switch (current->value.type) {
 				case SCS_VALUE_TYPE_INVALID: {
-					json[current->name] = nullptr;
+					json[name] = nullptr;
 					break;
 				}
 				case SCS_VALUE_TYPE_bool: {
-					json[current->name] = current->value.value_bool.value;
+					json[name] = current->value.value_bool.value;
 					break;
 				}
 				case SCS_VALUE_TYPE_s32: {
-					json[current->name] = current->value.value_s32.value;
+					json[name] = current->value.value_s32.value;
 					break;
 				}
 				case SCS_VALUE_TYPE_u32: {
-					json[current->name] = current->value.value_u32.value;
+					json[name] = current->value.value_u32.value;
 					break;
 				}
 				case SCS_VALUE_TYPE_s64: {
-					json[current->name] = current->value.value_s64.value;
+					json[name] = current->value.value_s64.value;
 					break;
 				}
 				case SCS_VALUE_TYPE_u64: {
-					json[current->name] = current->value.value_u64.value;
+					json[name] = current->value.value_u64.value;
 					break;
 				}
 				case SCS_VALUE_TYPE_float: {
-					json[current->name] = current->value.value_float.value;
+					json[name] = current->value.value_float.value;
 					break;
 				}
 				case SCS_VALUE_TYPE_double: {
-					json[current->name] = current->value.value_double.value;
+					json[name] = current->value.value_double.value;
 					break;
 				}
 				case SCS_VALUE_TYPE_fvector: {
 					auto vec = current->value.value_fvector;
-					json[current->name]["x"] = vec.x;
-					json[current->name]["y"] = vec.y;
-					json[current->name]["z"] = vec.z;
+					json[name]["x"] = vec.x;
+					json[name]["y"] = vec.y;
+					json[name]["z"] = vec.z;
 					break;
 				}
 				case SCS_VALUE_TYPE_dvector: {
 					auto vec = current->value.value_dvector;
-					json[current->name]["x"] = vec.x;
-					json[current->name]["y"] = vec.y;
-					json[current->name]["z"] = vec.z;
+					json[name]["x"] = vec.x;
+					json[name]["y"] = vec.y;
+					json[name]["z"] = vec.z;
 					break;
 				}
 				case SCS_VALUE_TYPE_euler: {
 					auto euler = current->value.value_euler;
-					json[current->name]["heading"] = euler.heading * 360.0f;
-					json[current->name]["pitch"] = euler.pitch * 360.0f;
-					json[current->name]["roll"] = euler.roll * 360.0f;
+					json[name]["heading"] = euler.heading * 360.0f;
+					json[name]["pitch"] = euler.pitch * 360.0f;
+					json[name]["roll"] = euler.roll * 360.0f;
 					break;
 				}
 				case SCS_VALUE_TYPE_fplacement: {
 					auto pos = current->value.value_fplacement.position;
 					auto ori = current->value.value_fplacement.orientation;
 
-					json[current->name]["position"]["x"] = pos.x;
-					json[current->name]["position"]["y"] = pos.y;
-					json[current->name]["position"]["z"] = pos.z;
+					json[name]["position"]["x"] = pos.x;
+					json[name]["position"]["y"] = pos.y;
+					json[name]["position"]["z"] = pos.z;
 
-					json[current->name]["orientation"]["heading"] = ori.heading * 360.0f;
-					json[current->name]["orientation"]["pitch"] = ori.pitch * 360.0f;
-					json[current->name]["orientation"]["roll"] = ori.roll * 360.0f;
+					json[name]["orientation"]["heading"] = ori.heading * 360.0f;
+					json[name]["orientation"]["pitch"] = ori.pitch * 360.0f;
+					json[name]["orientation"]["roll"] = ori.roll * 360.0f;
 					break;
 				}
 				case SCS_VALUE_TYPE_dplacement: {
 					auto pos = current->value.value_dplacement.position;
 					auto ori = current->value.value_dplacement.orientation;
 
-					json[current->name]["position"]["x"] = pos.x;
-					json[current->name]["position"]["y"] = pos.y;
-					json[current->name]["position"]["z"] = pos.z;
+					json[name]["position"]["x"] = pos.x;
+					json[name]["position"]["y"] = pos.y;
+					json[name]["position"]["z"] = pos.z;
 
-					json[current->name]["orientation"]["heading"] = ori.heading * 360.0f;
-					json[current->name]["orientation"]["pitch"] = ori.pitch * 360.0f;
-					json[current->name]["orientation"]["roll"] = ori.roll * 360.0f;
+					json[name]["orientation"]["heading"] = ori.heading * 360.0f;
+					json[name]["orientation"]["pitch"] = ori.pitch * 360.0f;
+					json[name]["orientation"]["roll"] = ori.roll * 360.0f;
 					break;
 				}
 				case SCS_VALUE_TYPE_string: {
-					json[current->name] = current->value.value_string.value;
+					json[name] = current->value.value_string.value;
 					break;
 				}
 				default: {
-					json[current->name] = "unknown";
+					json[name] = "unknown";
 					break;
 				}
 			}
